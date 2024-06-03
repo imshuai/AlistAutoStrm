3113ee6ff2ce3da3cd2d409a1ca19b3dad2505e2
iris-lee <iris-me@live.com>
2024-06-03 18:09:14 +0800

    添加strm类型的必要函数

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require github.com/urfave/cli/v2 v2.27.1
 require golang.org/x/sys v0.19.0 // indirect
 
 require (
+	github.com/boltdb/bolt v1.3.1
 	github.com/cpuguy83/go-md2man/v2 v2.0.4 // indirect
 	github.com/imshuai/alistsdk-go v1.0.1
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
--- types.go
+++ types.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"crypto/md5"
+	"encoding/json"
 	"fmt"
 	"os"
+
+	"github.com/boltdb/bolt"
+)
+
+var (
+	db *bolt.DB
 )
 
 type Strm struct {
@@ -17,14 +24,57 @@ func (s Strm) Key() string {
 	return fmt.Sprintf("%x", byts)
 }
 
+func (s Strm) Value() []byte {
+	byts, _ := json.Marshal(s)
+	return byts
+}
+
 func (s Strm) Save() error {
-	return nil
+	//TODO 使用boltdb实现Strm对象的保存逻辑
+	return db.Update(func(tx *bolt.Tx) error {
+		//创建一个名为"strm"的bucket
+		b, err := tx.CreateBucketIfNotExists([]byte("strm"))
+		if err != nil {
+			return err
+		}
+		//将Strm对象的key和value存入bucket中
+		return b.Put([]byte(s.Key()), s.Value())
+	})
 }
 
 func (s Strm) GenStrm() error {
+	//创建s.Dir目录
 	err := os.MkdirAll(s.Dir, 0666)
 	if err != nil {
 		return err
 	}
+	//将s.RawURL写入s.Dir目录下的s.Name文件中
 	return os.WriteFile(s.Dir+"/"+s.Name, []byte(s.RawURL), 0666)
+}
+
+func GetStrm(rawUrl string) (*Strm, error) {
+	//TODO 使用boltdb实现根据key获取Strm对象的逻辑
+	var strm Strm
+
+	// 计算key
+	key := fmt.Sprintf("%x", md5.Sum([]byte(rawUrl)))
+
+	err := db.View(func(tx *bolt.Tx) error {
+		// 获取名为strm的bucket
+		b := tx.Bucket([]byte("strm"))
+		if b == nil {
+			// 如果bucket不存在，返回错误
+			return fmt.Errorf("bucket not found")
+		}
+		// 根据key获取value
+		v := b.Get([]byte(key))
+		if v == nil {
+			// 如果key不存在，返回错误
+			return fmt.Errorf("key not found")
+		}
+		// 将value反序列化为Strm对象
+		return json.Unmarshal(v, &strm)
+	})
+	// 返回Strm对象和错误
+	return &strm, err
 }
